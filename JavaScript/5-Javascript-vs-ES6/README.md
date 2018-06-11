95a48e1d08adb70dd52fd739f611603a586e9e89
tjshiu <tiffany.shiu.2@gmail.com>
2018-06-11 14:42:01 -0700

    'classes' in JavaScript

--- JavaScript/5-Javascript-vs-ES6/README.md
+++ JavaScript/5-Javascript-vs-ES6/README.md
@@ -115,3 +115,94 @@ __4. Classes__
 In concept, there is no such thing as a "class" or a blueprint in JS like it is in other OO languages like Java. However, many people have treated the constructor function that creates Object when we use the "new" keyword as classes.
 
 __ES5__
+``` JavaScript
+var Shape = function(id, x, y) {
+  this.id = id;
+  this.location(x, y);
+};
+
+Shape.prototype.location = function(x, y) {
+  this.x = x;
+  this.y = y;
+};
+
+Shape.prototype.toString = function() {
+  return "Shape(" + this.id + ")";
+};
+
+Shape.prototype.getLocation = function() {
+  return {
+    x: this.x,
+    y: this.y,
+  };
+};
+
+var Circle = function(id, x, y, radius) {
+  Shape.call(this, id, x, y);
+  this.radius = radius;
+};
+
+Circle.prototype = Object.create(Shape.prototype);
+Circle.prototype.constructor = Circle;
+
+Circle.defaultCircle = function() { //static function
+  return new Circle("default", 0, 0, 100);
+};
+
+Circle.prototype.toString = function () {
+  return "Circle > " + Shape.prototype.toString.call(this);
+};
+
+var defaultCircle = Circle.defaultCircle(); // call static function
+
+var myCircle = new Circle('123', '5px', '10px', 5); // create new instance
+console.log(myCircle.toString()); //Circle > Shape(123)
+console.log(myCircle.getLocation()); // {x: '5px', y: '10px'}
+```
+
+__ES6__
+``` JavaScript
+class Shape {
+  constructor(id, x, y) {
+    this.id = id;
+    this.location(x, y);
+  }
+
+  location(x, y) { // <--- prototype function
+    this.x = x;
+    this.y = y;
+  }
+
+  toString() { // <-- prototype function
+    return "Shape(" + this.id + ")";
+  }
+
+  getLocation() {
+    return {
+      x: this.x,
+      y: this.y
+    };
+  }
+}
+
+class Circle extends Shape {
+  constructor(id, x, y, radius) {
+    super(id, x, y); // Call Shape's constructor (via super)
+    this.radius = radius;
+  }
+
+  static defaultCircle() { // Static function
+    return new Circle("default", 0, 0, 100);
+  }
+
+  toString() { // <- override toString of Shape
+    return "Circle > " + super.toString(); // call "super" instead of "this" to access parent
+  }
+}
+
+var defaultCircle = Circle.defaultCircle(); // call static function
+
+var myCircle = new Circle('123', '5px', '10px', 5); // create new instance
+console.log(myCircle.toString()); //Circle > Shape(123)
+console.log(myCircle.getLocation()); // {x: '5px', y: '10px'}
+```
--- JavaScript/5-Javascript-vs-ES6/test.js
+++ JavaScript/5-Javascript-vs-ES6/test.js
@@ -1,12 +1,86 @@
-var arr = [];
-for (let i = 0; i < 3; i++) { // let creates new "i" for each loop
-  arr.push(function () {
-    return i; // refers to local "i"
-  });
+// var Shape = function(id, x, y) {
+//   this.id = id;
+//   this.location(x, y);
+// };
+//
+// Shape.prototype.location = function(x, y) {
+//   this.x = x;
+//   this.y = y;
+// };
+//
+// Shape.prototype.toString = function() {
+//   return "Shape(" + this.id + ")";
+// };
+//
+// Shape.prototype.getLocation = function() {
+//   return {
+//     x: this.x,
+//     y: this.y,
+//   };
+// };
+//
+// var Circle = function(id, x, y, radius) {
+//   Shape.call(this, id, x, y);
+//   this.radius = radius;
+// };
+//
+// Circle.prototype = Object.create(Shape.prototype);
+// Circle.prototype.constructor = Circle;
+//
+// Circle.defaultCircle = function() { //static function
+//   return new Circle("default", 0, 0, 100);
+// };
+//
+// Circle.prototype.toString = function () {
+//   return "Circle > " + Shape.prototype.toString.call(this);
+// };
+//
+// var defaultCircle = Circle.defaultCircle(); // call static function
+//
+// var myCircle = new Circle('123', '5px', '10px', 5); // create new instance
+// console.log(myCircle.toString()); //Circle > Shape(123)
+// console.log(myCircle.getLocation()); // {x: '5px', y: '10px'}
+
+class Shape {
+  constructor(id, x, y) {
+    this.id = id;
+    this.location(x, y);
+  }
+
+  location(x, y) { // <--- prototype function
+    this.x = x;
+    this.y = y;
+  }
+
+  toString() { // <-- prototype function
+    return "Shape(" + this.id + ")";
+  }
+
+  getLocation() {
+    return {
+      x: this.x,
+      y: this.y
+    };
+  }
 }
 
-// console.log(i); // reference error (since there is no "i" in global space)
+class Circle extends Shape {
+  constructor(id, x, y, radius) {
+    super(id, x, y); // Call Shape's constructor (via super)
+    this.radius = radius;
+  }
 
-for (var j = 0; j < 3; j++) {
-  console.log(arr[j]()) // prints 0, 1, and 2
+  static defaultCircle() { // Static function
+    return new Circle("default", 0, 0, 100);
+  }
+
+  toString() { // <- override toString of Shape
+    return "Circle > " + super.toString(); // call "super" instead of "this" to access parent
+  }
 }
+
+var defaultCircle = Circle.defaultCircle(); // call static function
+
+var myCircle = new Circle('123', '5px', '10px', 5); // create new instance
+console.log(myCircle.toString()); //Circle > Shape(123)
+console.log(myCircle.getLocation()); // {x: '5px', y: '10px'}
